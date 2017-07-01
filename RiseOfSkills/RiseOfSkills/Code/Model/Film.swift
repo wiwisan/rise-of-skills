6945c0f217a46d7eead6cfea08a3ba66e2bd6118
Wildine Anthony <wildine.anthony@gmail.com>
2017-07-01 15:57:43 +0200

    Start Film Model

---
+++ RiseOfSkills/RiseOfSkills/Code/Model/Film.swift
@@ -0,0 +1,25 @@
+//  Film.swift
+//  RiseOfSkills
+//
+//  Copyright © 2017 Wildine Anthony. All rights reserved.
+
+
+import RealmSwift
+
+final class Film: Object {
+  
+  dynamic var title = ""
+  dynamic var episodeId = ""
+  dynamic var openingCrawl = ""
+  dynamic var director = ""
+  dynamic var producer = ""
+  dynamic var releaseDate = ""
+  dynamic var species = ""
+  dynamic var starships = ""
+  dynamic var vehicles = ""
+  dynamic var characters = ""
+  dynamic var planets = ""
+  dynamic var url = ""
+  dynamic var created = ""
+  dynamic var edited = ""
+}
