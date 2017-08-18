43be9932557b9f86827bffc8ee002af77259e44e
Wildine Anthony <wildine@smartnsoft.com>
2017-08-18 11:03:11 +0200

    Add UIColor extention

--- RiseOfSkills/RiseOfSkills/Code/AppDelegate/AppDelegate+Appearance.swift
+++ RiseOfSkills/RiseOfSkills/Code/AppDelegate/AppDelegate+Appearance.swift
@@ -21,7 +21,7 @@ extension AppDelegate {
     let appearance = UINavigationBar.appearance()
     appearance.barStyle = UIBarStyle.black
     appearance.isTranslucent = false
-    let backgroundColor = self.hexStringToUIColor(hex: "#00BAFF")
+    let backgroundColor = UIColor.init(hexString: "#00BAFF")
     appearance.tintColor = .white
     appearance.barTintColor = backgroundColor
     appearance.setBackgroundImage(UIImage(), for: .default)
@@ -31,27 +31,5 @@ extension AppDelegate {
     let titleTextAttributes: [String: Any] = [NSForegroundColorAttributeName: UIColor.white,
                                               NSFontAttributeName: font ?? .systemFont(ofSize: 24)]
     appearance.titleTextAttributes = titleTextAttributes
-  }
-  
-  func hexStringToUIColor (hex:String) -> UIColor {
-    var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-    
-    if (cString.hasPrefix("#")) {
-      cString.remove(at: cString.startIndex)
-    }
-    
-    if ((cString.characters.count) != 6) {
-      return UIColor.gray
-    }
-    
-    var rgbValue:UInt32 = 0
-    Scanner(string: cString).scanHexInt32(&rgbValue)
-    
-    return UIColor(
-      red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-      green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-      blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-      alpha: CGFloat(1.0)
-    )
   }
 }
