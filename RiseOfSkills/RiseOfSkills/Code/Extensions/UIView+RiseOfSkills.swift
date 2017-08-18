829df4965a2a047d9f274000585a8ce2493a7d8b
Wildine Anthony <wildine@smartnsoft.com>
2017-08-18 11:43:20 +0200

    Add UIView Extension for Grandient color

---
+++ RiseOfSkills/RiseOfSkills/Code/Extensions/UIView+RiseOfSkills.swift
@@ -0,0 +1,24 @@
+//
+//  UIView+RiseOfSkills.swift
+//  RiseOfSkills
+//
+//  Copyright © 2017 Wildine Anthony. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+  
+  func setGradientBackground() {
+    let colorTop =  UIColor.init(hexString: "#21D0FD").cgColor
+    let colorBottom = UIColor.init(hexString: "#EE4DB9").cgColor
+    
+    let gradientLayer = CAGradientLayer()
+    gradientLayer.colors = [ colorTop, colorBottom]
+    gradientLayer.locations = [ 0.0, 1.0]
+    gradientLayer.frame = self.bounds
+    
+    self.layer.addSublayer(gradientLayer)
+  }
+}
